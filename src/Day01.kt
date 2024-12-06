b75c00aa6af4a8fdfd420f24399407ed67818ce5
Marcin Opaliński <marcinopalinskito@gmail.com>
2024-12-06 12:34:28 +0100

    Implement puzzles for the first day

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,21 +1,64 @@
+import kotlin.math.abs
+
 fun main() {
+
+    fun prepareList(input: List<String>): Pair<MutableList<Int>, MutableList<Int>> {
+        val list1: MutableList<Int> = mutableListOf()
+        val list2: MutableList<Int> = mutableListOf()
+
+        for (line in input) {
+            val num1 = line.split(' ').first()
+            val num2 = line.split(' ').last()
+
+            list1.add(num1.toInt())
+            list2.add(num2.toInt())
+        }
+
+        list1.sort()
+        list2.sort()
+
+        return Pair(list1, list2)
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        val (list1, list2) = prepareList(input)
+
+        var totalDistance = 0
+
+        for (i in list1.indices) {
+            totalDistance += abs(list1[i] - list2[i])
+        }
+
+        return totalDistance
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val (list1, list2) = prepareList(input)
+
+        var similarityScore = 0
+
+        for (num1 in list1) {
+            var repetitions = 0
+            for (num2 in list2) {
+                if (num1 == num2) {
+                    repetitions++
+                }
+            }
+            similarityScore += repetitions  * num1
+        }
+
+        return similarityScore
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
 
-    // Or read a large test input from the `src/Day01_test.txt` file:
+    // Or read a test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+//    println(testInput)
+//    part1(testInput).println()
+    part2(testInput).println()
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
-    part1(input).println()
+//    part1(input).println()
     part2(input).println()
 }
